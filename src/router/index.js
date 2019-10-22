19ce5634e1293a97eb993adc1aab68ad2ad39b57
Joshua1gd <joshua1gd@yahoo.com>
2019-10-21 17:11:00 -0700

    finished setup and installing bootstrap

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Shop from '../views/Shop.vue'
+import NewEntry from '../views/NewEntry.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home
+    name: 'shop',
+    component: Shop
   },
   {
     path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    name: 'newentry',
+    component: NewEntry
   }
 ]
 
---
+++ src/views/Shop.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <ShopItem msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ShopItem from '@/components/ShopItem.vue'
+
+export default {
+  name: 'shopitem',
+  components: {
+    ShopItem
+  }
+}
+</script>
